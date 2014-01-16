e5c23171d018fcbbf8d28e1c16d9b56313c01536
laanak <laanak@gmail.com>
2014-01-16 14:08:03 -0500

    fixed files

--- bin/cleaner.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-date >> /cpm/projects/smapi/logs/cleaner.log 2>&1
-/usr/bin/find /cpm/projects/smapi/data/bmarks -amin +180 -delete >> /cpm/projects/smapi/logs/cleaner.log 2>&1
-/usr/bin/find /cpm/projects/smapi/data/bmarks -mmin +240 -delete >> /cpm/projects/smapi/logs/cleaner.log 2>&1
-
--- bin/stopper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for COPY in '003' '004' '009' '010' '011' '012' '013' '016' '017' '018' '019' '020' '005'
-do
-	echo 0 > /cpm/projects/smapi/state/state_${COPY}.txt
-done
