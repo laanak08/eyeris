7f492f7739c7bfddb350ab30ca34c2d46e7839a9
UNIX CPM Account <cpm@lab7983.(none)>
2014-01-24 11:28:57 -0500

    improving querypage

--- views/QueryPage.py
+++ views/QueryPage.py
@@ -5,11 +5,9 @@ from django.shortcuts import render_to_response
 from django import forms
 
 class QueryForm(forms.Form):
-	dimeName = forms.CharField(required=False)
-	serverName = forms.CharField(required=False)
-	dimeName = forms.CharField(required=False)
-	app = forms.CharField(required=False)
-	hcd = forms.CharField(required=False)
+	application = forms.CharField(required=False)
+	server = forms.CharField(required=False)
+	hierarchCode = forms.CharField(required=False)
 	transId = forms.CharField(required=False)
 	webId = forms.CharField(required=False)
 	action = forms.CharField(required=False)
@@ -18,12 +16,19 @@ def render_query_page(request):
 	if request.method == 'POST':
 		form = QueryForm(request.POST)
 		if form.is_valid():
-			# process form
-			dn = form.cleaned_data['dimeName']
-			return render_to_response('ResultsPage.html', {'message' : dn})
+			query = form
+			queryResults = process_form(form.cleaned_data)
+			return render_to_response('ResultsPage.html', {'query' : query, 'queryResults' : queryResults })
 	else:
 		form = QueryForm()
 
 	return render_to_response('QueryPage.html', 
-		{'form' : form }, 
+		{'form' : form,
+#			'alert' : 'This is a prototype only on a select set of servers. Records are kept for 24 hours only',
+			'updates' : "All identified LexisAdvance servers are now loading."+
+				" Begin and end time are displaying as columns, as well as response time in seconds."+
+				" The Server filter is set to auto-complete with the servers we have." }, 
 		context_instance=RequestContext(request) )
+
+def process_form(form):
+	return form['application'] 
